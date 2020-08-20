6b5bc5906f3ceda47a74322dfeec9d826c63fcab
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-08-20 09:24:14 -0300

    moked jest

--- api/src/clients/stormGlass.ts
+++ api/src/clients/stormGlass.ts
@@ -39,6 +39,11 @@ export class StormGlass {
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     const response = await this.request.get<StormGlassForecastResponse>(
       `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+      {
+        headers:{
+          Authorization: 'fake-token',
+        }
+      }
     );
 
     return this.normalizeResponse(response.data);
