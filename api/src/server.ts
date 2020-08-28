656c6f1270b4aa3c58681d9021893766cf962e85
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-08-28 10:10:22 -0300

    adição do mongo

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import { Server } from '@overnightjs/core';
 import { Application } from 'express';
 import bodyParser from 'body-parser';
+import * as database from '@src/database';
 
 import ForecastController from './controllers/forecast';
 import './util/module-alias';
@@ -10,9 +11,10 @@ export class SetupServer extends Server {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.stupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -22,6 +24,14 @@ export class SetupServer extends Server {
   private stupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
