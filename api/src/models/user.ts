bd5b47d58896bfe8955de168f734cd262ff2cd5c
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-10-01 09:58:40 -0300

    hash

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose, { Document, Model } from 'mongoose';
 
+import AuthService from '@src/services/auth';
+
 export enum CUSTOM_VALIDATION {
   DUPLICATED = 'DUPLICATED',
 }
@@ -42,5 +44,17 @@ schema.path('email').validate(
   'already exists in the database.',
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (err) {
+    console.error(`Error hashing the password for the user ${this.name}`);
+  }
+});
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
