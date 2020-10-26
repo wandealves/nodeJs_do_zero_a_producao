52739d0ddc6d5826830cb43a68487e5dcc5249c8
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-10-26 09:29:03 -0300

    Graceful shutdown e tratamento de erros em Node.js

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,42 @@
 import { SetupServer } from './server';
 import config from 'config';
+import logger from './logger';
+
+enum ExitStatus{
+  Failure = 1,
+  Sucess = 0,
+}
+
+process.on('unhandledRejection', (reason, promise)=>{
+  logger.error(`App exiting due to an unhandled promise: ${promise} and reason: ${reason}`);
+  throw reason;
+});
+
+process.on('uncaughtException', (error)=>{
+  logger.error(`App exiting due to an uncaught exception: ${error}`);
+  process.exit(ExitStatus.Failure);
+});
 
 (async (): Promise<void> => {
+  try{
   const server = new SetupServer(config.get('App'));
   await server.init();
   server.start();
+
+  const exitSignals : NodeJS.Signals[] = ['SIGINT','SIGTERM','SIGQUIT'];
+  exitSignals.map((sig) => process.on(sig, async() => {
+    try{
+      await server.close();
+      logger.info(`App exited with sucess`);
+      process.exit(ExitStatus.Sucess);
+    }catch(error){
+      logger.error(`App exited with error: ${error}`);
+      process.exit(ExitStatus.Failure);
+    }
+  }));
+
+  }catch(error){
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
 })();
